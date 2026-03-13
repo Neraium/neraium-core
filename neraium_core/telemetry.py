1549ca6c61a5819416a36fe356c385c13c467e31
Neraium <neraiumai@gmail.com>
2026-03-13 11:21:14 -0500

    Delete telemetry.py

--- neraium_core/telemetry.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import Dict
-from datetime import datetime
-
-
-class TelemetryPayload(BaseModel):
-    timestamp: datetime
-    signals: Dict[str, float]
