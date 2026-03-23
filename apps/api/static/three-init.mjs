30c2380cef39c41abe10e70921e1cf28c2caf97b
Craig <craig@neraium.com>
2026-03-23 00:21:46 -0500

    upgrade ui

--- apps/api/static/three-init.mjs
+++ apps/api/static/three-init.mjs
@@ -5,9 +5,16 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { RoomEnvironment } from "three/addons/environments/RoomEnvironment.js";
+import { Line2 } from "three/addons/lines/Line2.js";
+import { LineGeometry } from "three/addons/lines/LineGeometry.js";
+import { LineMaterial } from "three/addons/lines/LineMaterial.js";
 import { PMREMGenerator } from "three/addons/utils/PMREMGenerator.js";
 
 window.__THREE_ESM = THREE;
 window.__OrbitControls = OrbitControls;
 window.__RoomEnvironment = RoomEnvironment;
 window.__PMREMGenerator = PMREMGenerator;
+/** Wide structural links (triangle strips, not GL line width). */
+window.__Line2 = Line2;
+window.__LineGeometry = LineGeometry;
+window.__LineMaterial = LineMaterial;
