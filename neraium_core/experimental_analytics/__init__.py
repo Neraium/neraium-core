fe483cc462cdb4b4a909664f3cc127056fa1b9e0
Neraium <neraiumai@gmail.com>
2026-03-24 17:57:24 -0500

    Add experimental horizon estimation analytics layer

--- neraium_core/experimental_analytics/__init__.py
+++ neraium_core/experimental_analytics/__init__.py
@@ -1,4 +1,5 @@
 from .constraint_analysis import analyze_constraint_lock_in
 from .trajectory_analysis import classify_trajectory_path
+from .horizon_analysis import estimate_risk_horizon
 
-__all__ = ["classify_trajectory_path", "analyze_constraint_lock_in"]
+__all__ = ["classify_trajectory_path", "analyze_constraint_lock_in", "estimate_risk_horizon"]
