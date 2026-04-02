e2cdb6c1eaaa708af2b3438f22bf4a7ed15d6ab1
Neraium <neraiumai@gmail.com>
2026-04-02 17:14:22 -0500

    Add stock market and betting adapters with market engine pipeline

--- neraium_core/__init__.py
+++ neraium_core/__init__.py
@@ -6,6 +6,12 @@ from neraium_core.sports_betting_adapter import build_betting_frame
 from neraium_core.stock_market_adapter import build_stock_frame
 from neraium_core.trading_signals import map_structural_output_to_signal
 
+
+def load_market_data(*args, **kwargs):
+    from neraium_core.market_data_loader import load_market_data as _load_market_data
+
+    return _load_market_data(*args, **kwargs)
+
 try:
     __version__ = version("neraium-core")
 except PackageNotFoundError:
@@ -21,5 +27,6 @@ __all__ = [
     "build_stock_frame",
     "build_betting_frame",
     "map_structural_output_to_signal",
+    "load_market_data",
     "__version__",
 ]
